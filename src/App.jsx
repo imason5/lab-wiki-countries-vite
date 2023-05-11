b572f9cb7bb464cf23fa93e6a5bacc284eadf752
imason5 <ianmason5@googlemail.com>
2023-05-11 15:50:51 +0100

    Add using API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import countriesJson from "./countries.json";
+// import countriesJson from "./countries.json";
 import Navbar from "./components/Navbar";
 import CountriesList from "./components/CountriesList";
 import CountryDetails from "./components/CountryDetails";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [countries, setCountries] = useState(countriesJson);
+  // const [countries, setCountries] = useState(countriesJson); // Initialize countries state with countriesJson. Comment in for Json use
+
+  const [countries, setCountries] = useState([]); // Initialize countries state with empty array, now fetch countries from API. Comment out for Json use
+
+  // Fetch countries from API - comment out for Json use
+  useEffect(() => {
+    fetch("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        // Sort countries alphabetically by common name
+        const sortedCountries = data.sort((a, b) =>
+          a.name.common.localeCompare(b.name.common)
+        );
+        setCountries(sortedCountries);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
     <div className="App">
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import { useParams, Link } from "react-router-dom";
 
 function CountryDetails({ countries }) {
   const { alpha3Code } = useParams(); // access the alpha3Code parameter from the URL
 
+  console.log(countries);
+  // function to check if countries is defined
+  if (!countries) {
+    return <div>Loading countries</div>;
+  }
+
   const country = countries.find(
     (country) => country.alpha3Code === alpha3Code
   ); // find the country with the alpha3Code from the URL
+
+  // function to check if country is defined
+  if (!country) {
+    return <div>Country not found</div>;
+  }
 
   // Find border countries
   const getBorderCountries = () => {
@@ -15,6 +25,7 @@ function CountryDetails({ countries }) {
         (country) => country.alpha3Code === borderCountryAlpha3Code // find the country with the alpha3Code from the URL if it matches the border country's alpha3Code
       );
 
+      // Return a Link component for each border country
       return (
         <li key={borderCountryAlpha3Code}>
           <Link to={`/${borderCountryAlpha3Code}`}>
